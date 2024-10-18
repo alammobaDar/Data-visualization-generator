d77c04eaf564ac4037a062805ea5dfd02812985d
Darren <edrichdarrensantuyo172030@gmail.com>
2024-10-18 23:51:32 +0800

    change the color theme

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sys
+
+from PyQt5.QtGui import QColor, QFontDatabase
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QFrame, \
-    QComboBox, QGridLayout, QSizePolicy
-from PyQt5.QtCore import Qt
+    QComboBox, QGridLayout, QSizePolicy, QGraphicsDropShadowEffect
+from PyQt5.QtCore import Qt, QFile, QTextStream
 
 import kind_of_plots
 from Table import Table
@@ -13,22 +15,36 @@ class UI(QMainWindow):
     def __init__(self):
         super().__init__()
 
+
+        # Create drop shadow effect
+        shadow = QGraphicsDropShadowEffect()
+        shadow.setBlurRadius(10)
+        shadow.setOffset(3, 3)
+        shadow.setColor(QColor(0, 0, 0, 150))
+
+        # Load some fonts
+        QFontDatabase.addApplicationFont(".Poppins/Poppins-Regular.ttf")
+        QFontDatabase.addApplicationFont(".Poppins/Poppins-Bold.ttf")
+
+
+        #Setting up the main window
         self.setWindowTitle("Matplotlib Generator")
-        self.setGeometry(100, 100, 1200, 900)
+        self.setGeometry(100, 100, 1200, 800)
 
         # Main widget to hold all layouts and widgets
         central_widget = QWidget(self)
         self.setCentralWidget(central_widget)
+
         main_layout = QVBoxLayout(central_widget)
 
         # Title bar section
         self.title_bar = QFrame(self)
         self.title_bar.setFixedHeight(100)
-        self.title_bar.setStyleSheet("background-color: gray;")
+        self.title_bar.setObjectName("title_bar")
         title_layout = QVBoxLayout(self.title_bar)
         self._title = QLabel("Data Visualization Tool", self)
         self._title.setAlignment(Qt.AlignCenter)
-        self._title.setStyleSheet("font-size: 25px; font-weight: bold; color: white;")
+        self._title.setObjectName("_title")
         title_layout.addWidget(self._title)
         main_layout.addWidget(self.title_bar)
 
@@ -38,14 +54,14 @@ class UI(QMainWindow):
 
         # Dashboard (left panel)
         self.dashboard = QFrame(self)
-        self.dashboard.setStyleSheet("background-color: gray;")
+        self.dashboard.setObjectName("dashboard")
         self.dashboard.setFixedWidth(450)
         dashboard_layout = QVBoxLayout(self.dashboard)
         content_layout.addWidget(self.dashboard)
 
         # Main section (right panel)
         self.main_section = QFrame(self)
-        self.main_section.setStyleSheet("background-color: white;")
+        self.main_section.setObjectName("main_section")
         content_layout.addWidget(self.main_section)
         self.main_section_layout = QVBoxLayout(self.main_section)
 
@@ -63,11 +79,18 @@ class UI(QMainWindow):
         # ComboBox for selecting plot type
         self.combo_box_frame = QFrame()
         combo_box_frame_layout = QVBoxLayout(self.combo_box_frame)
-        self.combo_box_frame.setStyleSheet("background-color: gray; padding: 0px; margin: 0px")
+        self.combo_box_frame.setObjectName("combo_box_frame")
+        self.combo_box_frame.setGraphicsEffect(shadow)
+
         self.select_label = QLabel("Please choose what kind of plot you want to use", self)
+        self.select_label.setProperty("class", "font_color")
+
         self.selected_plot = QComboBox(self)
         self.selected_plot.addItems(["Plot", "Hist", "Scatter", "Bar", "Pie"])
         self.selected_plot.currentIndexChanged.connect(self.on_combo_box)
+        self.selected_plot.setObjectName("selected_plot")
+        self.selected_plot.setGraphicsEffect(shadow)
+
 
         combo_box_frame_layout.addWidget(self.select_label, alignment=Qt.AlignCenter | Qt.AlignTop)
         combo_box_frame_layout.addWidget(self.selected_plot, alignment=Qt.AlignCenter | Qt.AlignTop)
@@ -75,7 +98,7 @@ class UI(QMainWindow):
         # creating a container for the kind_of_plots to be placed
         self.plots_frame = QFrame()
         plots_frame_layout = QVBoxLayout(self.plots_frame)
-        self.plots_frame.setStyleSheet("background-color: gray; padding: 0px; margin: 0px")
+        self.plots_frame.setObjectName("dashboard")
 
         # Creating instances of plots
         self.pl = kind_of_plots
@@ -103,6 +126,17 @@ class UI(QMainWindow):
 
         #instantiate the table class
         self.tb = Table()
+
+        #load the stylesheet
+        self.load_stylesheet()
+
+    def load_stylesheet(self):
+        file = QFile("styles.qss")
+        if file.open(QFile.ReadOnly | QFile.Text):
+            stream = QTextStream(file)
+            self.setStyleSheet(stream.readAll())
+
+
     def erase_frame(self):
         # Clear plot frames when switching between plot types
         self.plot_instance.plot_frame.setVisible(False)
